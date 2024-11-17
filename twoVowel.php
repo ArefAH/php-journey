4ed27691784a4b42b675028af94291a31bd84271
ArefAH <arefabouhamdan@gmail.com>
2024-11-17 21:55:01 +0200

    PHP - Added add node function

--- twoVowel.php
+++ twoVowel.php
@@ -8,12 +8,25 @@ class Node {
         $this->next = null;
     }
 }
-
 class LinkedList {
     public $head;
 
     public function __construct() {
         $this->head = null;
+    }
+
+    public function add($value) {
+        $newNode = new Node($value);
+        if ($this->head === null) {
+            $this->head = $newNode;
+        } 
+        else {
+            $current = $this->head;
+            while ($current->next !== null) {
+                $current = $current->next;
+            }
+            $current->next = $newNode;
+        }
     }
 }
 
